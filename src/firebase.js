1e3e29f2744727f4321654b448977df807993e0a
Sinisa <sinisha.stojanovic@gmail.com>
2023-01-19 16:49:15 +0100

    Firebase API installed and Chat app firebase config added to project

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_9w0SOAJ-yvvnvPEqMsaYalgco4iXym8",
+  authDomain: "chat-ad8b8.firebaseapp.com",
+  projectId: "chat-ad8b8",
+  storageBucket: "chat-ad8b8.appspot.com",
+  messagingSenderId: "483751395188",
+  appId: "1:483751395188:web:d34a47ec6ecf394b617621",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
