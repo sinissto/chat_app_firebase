2b42a0f744fbbf79fe4e7f113bbe6061f9bbc234
Sinisa <sinisha.stojanovic@gmail.com>
2023-01-20 00:45:21 +0100

    Register logic implemented, React-router-dom added to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style.scss";
 import AddAvatar from "../img/addAvatar.png";
+import { auth, storage, db } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const handleRegister = (e) => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
     e.preventDefault();
+
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      await uploadBytesResumable(storageRef, file);
+
+      const downloadURL = await getDownloadURL(storageRef);
+
+      await updateProfile(res.user, {
+        displayName,
+        photoURL: downloadURL,
+      });
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        uid: res.user.uid,
+        displayName,
+        email,
+        photoURL: downloadURL,
+      });
+
+      //create empty user chats on firestore
+      await setDoc(doc(db, "userChats", res.user.uid), {});
+
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+      console.log(err);
+    }
   };
 
   return (
@@ -19,10 +62,11 @@ const Register = () => {
           <input style={{ display: "none" }} type={"file"} id={"file"} />
           <label htmlFor={"file"}>
             <img src={AddAvatar} alt="" />
-            <spam>Add Avatar</spam>
+            <span>Add Avatar</span>
           </label>
           <button type={"submit"}>Sign Up</button>
         </form>
+        {err && <span>Something went wrong</span>}
         <p>You do have an account? Login</p>
       </div>
     </div>
